8b2651c4885d866b5734e73a2247294723a74ce5
Yeqing Li <yeqing.li@mavs.uta.edu>
2015-11-30 16:07:29 -0600

    Add handwritten dataset.

--- get_data.sh
+++ get_data.sh
@@ -3,6 +3,7 @@
 
 echo "Downloading..."
 
+wget --no-check-certificate https://www.dropbox.com/s/b00cdxc2s96o1to/handwritten.mat
 wget --no-check-certificate https://www.dropbox.com/s/ulvatoo8gepcdfk/Caltech101-7.mat
 wget --no-check-certificate https://www.dropbox.com/s/n5ld7mqhe3io960/Caltech101-20.mat 
 wget --no-check-certificate https://www.dropbox.com/s/88hbin8x4clgwii/Caltech101-all.mat 
